b5f7fdcb27e7bc32ab0165298c1580adaf3a46fa
dcw <d.white@imperial.ac.uk>
2020-02-06 23:15:03 +0000

    significant changes to 05.newdisplay - generate the solution as well as the game, and convert ps2eps too

--- 05.newdisplay/makeps.pl
+++ 05.newdisplay/makeps.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/perl
 #
-#	makeps: 	Reads the output of the reverse mastermind solver,
+#	makeps: 	Reads the output of the 02.genpuzzle/genpuzzle program
 #			specifically the final line, of the form
 #	"holes=4, colours=rgby: shortest=2, secret=bgrb, shortest guesses=grbb,rbgb, shortest scores=1-3,1-3"
-#			and writes a corresponding inc file ready to
-#			generate Postscript for that precise case..
+#			and generates an Encapsulated Postscript image
+#			for that precise case (via several intermediate forms)..
+#			and, now, a second Encapsulated Postscript image
+#			showing the secret code for the same case.
 
 
 use strict;
@@ -14,11 +16,6 @@ use Function::Parameters;
 
 die "Usage: makeps basename\n" unless @ARGV==1;
 my $basename = shift;
-
-my $incfile = "$basename.inc";
-my $psfile = "$basename.ps";
-
-open( my $outfh, '>', $incfile ) || die "makeps: can't create $incfile\n";
 
 my( $nholes, $colours, $minn, $secret, $g, $bw );
 
@@ -38,44 +35,24 @@ die "makeps: nholes $nholes must be > 1\n" unless $nholes > 1;
 
 say "nholes=$nholes, colours=$colours, ncolours=$ncolours, minn=$minn, secret=$secret, g=$g, bw=$bw";
 
-my @g = split( /,/, uc($g) );
-my $ng = @g;
 
-my @bw = split( /,/, $bw );
-my $nbw = @bw;
-
-die "makeps: shortest guesses has $minn guesses, not $ng\n"
-	unless $ng == $minn;
-
-die "makeps: shortest guesses has $minn guesses, not $nbw blacks+white scores\n"
-	unless $nbw == $minn;
-
-my $mg = $ng+1;
-
-my @sc;
-my @gbw;
-
-foreach my $gno (1..$ng)
+#
+# my $epsfilename = write_inc_file( $basename, $defns, $calls );
+#	Write a Postscript-with-includes file with $defns and $calls,
+#	then expand the includes and convert it to EPS.  Return the
+#	filename of the EPSfile (should be $basename.eps).
+#
+fun write_inc_file( $basename, $defns, $calls )
 {
-	my $g = $g[$gno-1];
-	my $bw = $bw[$gno-1];
-	$bw =~ /^(\d+)-(\d+)$/;
-	my( $b, $w ) = ( $1, $2 );
-	push @sc, "b$gno w$gno";
-	push @gbw,
-qq(/g$gno ($g) def
-/b$gno $b def
-/w$gno $w def
+	my $incfile = "$basename.inc";
+	my $psfile = "$basename.ps";
+	my $epsfile = "$basename.eps";
 
-);
+	open( my $outfh, '>', $incfile ) ||
+		die "makeps: can't create $incfile\n";
 
-}
-
-my $gstr = join( " ", map { "g$_" } 1..$ng );
-my $bwstr = join( " ", @sc );
-my $gbwtxt = join( "\n", @gbw );
-
-print $outfh qq(%!PS-Adobe-3.0
+	print $outfh
+qq(%!PS-Adobe-3.0
 %%Pages: 1
 %%EndComments
 
@@ -86,17 +63,9 @@ print $outfh qq(%!PS-Adobe-3.0
 %include "pslib/max"
 %include "pslib/colours"
 
-% Shhhhhhhhh! The secret code is $secret
-
-/maxguesses $mg  def
-/numholes   $nholes  def
-
-
-$gbwtxt
-
+$defns
 
 %include "mmsupport.i"
-
 
 %%Page: 1 1
 %%PageOrientation: Portrait
@@ -105,16 +74,123 @@ $gbwtxt
 
 /texth fontheight def
 
+$calls
+
+showpage
+);
+
+	close( $outfh );
+
+	system( "./inc $incfile $psfile" );
+
+	unlink( $epsfile ) if -f $epsfile;
+
+	system( "ps2eps $psfile" );
+
+	return $epsfile;
+}
+
+
+#
+# my $epsfile = makepuzzle( $basename );
+#	Write out the $basename.inc postscript-with-includes file
+#	that will show the entire puzzle, then convert it to EPS
+#	and return the filename of the EPS file.
+#
+fun makepuzzle( $basename )
+{
+	my @g = split( /,/, uc($g) );
+	my $ng = @g;
+
+	my @bw = split( /,/, $bw );
+	my $nbw = @bw;
+
+	die "makeps: shortest guesses has $minn guesses, not $ng\n"
+		unless $ng == $minn;
+
+	die "makeps: shortest guesses has $minn guesses, not $nbw blacks+white scores\n"
+		unless $nbw == $minn;
+
+	my $mg = $ng+1;
+
+	my @sc;
+	my @gbw;
+
+	foreach my $gno (1..$ng)
+	{
+		my $g = $g[$gno-1];
+		my $bw = $bw[$gno-1];
+		$bw =~ /^(\d+)-(\d+)$/;
+		my( $b, $w ) = ( $1, $2 );
+		push @sc, "b$gno w$gno";
+		push @gbw,
+qq(/g$gno ($g) def
+/b$gno $b def
+/w$gno $w def
+
+);
+	}
+
+	my $gstr = join( " ", map { "g$_" } 1..$ng );
+	my $bwstr = join( " ", @sc );
+	my $gbwstr = join( "\n", @gbw );
+
+	my $defns =
+qq(
+% Shhhhhhhhh! The secret code is $secret
+
+/maxguesses $mg  def
+/numholes   $nholes  def
+
+$gbwstr
+);
+
+	my $calls =
+qq(
 maxguesses shownumbers
 
 [ $gstr (     ) ]    showguesses
 
 [ $bwstr ]    showscores
-
-showpage
 );
 
-close( $outfh );
+	my $epsfile = write_inc_file( $basename, $defns, $calls );
 
-system( "./inc $incfile $psfile" );
-system( "gv $psfile" );
+	return $epsfile;
+}
+
+
+#
+# my $epsfile = makesecret( $basename );
+#	Write out the $basename.inc postscript-with-includes file
+#	that will show the secret code, then convert it to EPS
+#	and return the filename of the EPS file.
+#
+fun makesecret( $basename )
+{
+	my $defns =
+qq(
+/maxguesses 1  def
+/numholes   $nholes  def
+
+/secret ($secret) def
+);
+
+	my $calls =
+qq(
+[ secret ]    showguesses
+);
+
+	my $epsfile = write_inc_file( $basename, $defns, $calls );
+
+	return $epsfile;
+}
+
+
+my $epsfile = makepuzzle( $basename );
+system( "gv $epsfile &" );
+
+my $sepsfile = makesecret( $basename.'s' );
+system( "gv -geom +600+10 $sepsfile &" );
+
+say "generated $epsfile and $sepsfile";
